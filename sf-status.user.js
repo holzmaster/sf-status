c310b2f2f8ad3c8f429097df763ddfdf1ee2cfeb
pr0nopoly <pr0nopoly@outlook.com>
2015-01-18 14:21:10 +0100

    Update for Server-Side-CSS
    cha0s hat das CSS in das CSS der Hauptseite uebernommen. Es ist also nur noch notwendig, das Template einzufuegen.

--- sf-status.user.js
+++ sf-status.user.js
@@ -4,7 +4,7 @@
 // @description Zeigt den SF-Status eines Bildes an.
 // @namespace	holzmaster
 // @include		http://pr0gramm.com*
-// @version		1.0.2
+// @version		1.1.0
 // @updateURL	https://raw.githubusercontent.com/pr0nopoly/sf-status/master/sf-status.user.js
 // @downloadURL	https://raw.githubusercontent.com/pr0nopoly/sf-status/master/sf-status.user.js
 // @copyright	2015+, holzmaster
@@ -13,27 +13,10 @@
 // ==/UserScript==
 
 (function() {
-	var extensionStyle = ".extension-flags { color: #FFF;	padding: 0 0.5em 0.1em 0.45em; /* 0.1em 0.5em 0.2em 0.45em; */	margin-left: 0.3em;	vertical-align: baseline;	text-transform: lowercase;	text-align: center;	text-shadow: 1px 1px 1px #7F7F7F;	line-height: 1;	font-weight: 700;	font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;	border-radius: 0.25em; }\n";
-	extensionStyle += ".extension-flags-1 { background-color: #5CB85C; } /* #67D932 */\n";
-	extensionStyle += ".extension-flags-2 { background-color: #F0AD4E; } /* #EE4D2E */\n";
-	extensionStyle += ".extension-flags-4 { background-color: #D9534F; } /* #F00 */\n";
-
-	function addStyle(css) {
-		var head = document.getElementsByTagName("head")[0];
-		if (!head)
-			return;
-
-		var style = document.createElement("style");
-		style.type = "text/css";
-		style.innerHTML = css;
-		head.appendChild(style);
-	};
-
 	$(function() {
 		p.View.Stream.Item.prototype.template =
 		p.View.Stream.Item.prototype.template
 			.replace('Bild googeln </a> </span> <?js } ?>',
-					 'Bild googeln </a> </span> <?js } ?> <span class="extension-flags extension-flags-{item.flags}">{p.Stream.FLAG_NAME[item.flags]}</span>');
-		addStyle(extensionStyle);
+					 'Bild googeln </a> </span> <?js } ?> <span class="flags flags-{item.flags}">{p.Stream.FLAG_NAME[item.flags]}</span>');
 	});
 })();
