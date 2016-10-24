8767a39fe1af63806c28e49322faa774b94b2865
holzmaster <holzmaster@outlook.com>
2016-10-24 21:36:24 +0200

    Using own style again
    ...since the on-site style was removed

--- sf-status.user.js
+++ sf-status.user.js
@@ -14,10 +14,24 @@
 // ==/UserScript==
 
 (function() {
+	var extensionStyle = ".flags { color: #FFF;	padding: 0 0.5em 0.1em 0.45em; /* 0.1em 0.5em 0.2em 0.45em; */	margin-left: 0.3em;	vertical-align: baseline;	text-transform: lowercase;	text-align: center;	text-shadow: 1px 1px 1px #7F7F7F;	line-height: 1;	font-weight: 700;	font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;	border-radius: 0.25em; }\n";
+	extensionStyle += ".flags-1 { background-color: #5CB85C; } /* #67D932 */\n";
+	extensionStyle += ".flags-2 { background-color: #F0AD4E; } /* #EE4D2E */\n";
+	extensionStyle += ".flags-4 { background-color: #D9534F; } /* #F00 */\n";
+
+	function addStyle(css) {
+		var head = document.getElementsByTagName("head")[0];
+		if (!head) return;
+		var style = document.createElement("style");
+		style.type = "text/css";
+		style.innerHTML = css;
+		head.appendChild(style);
+	};
 	$(function() {
 		p.View.Stream.Item.prototype.template =
 		p.View.Stream.Item.prototype.template
 			.replace('Bild googeln </a> </span> <?js } ?>',
 					 'Bild googeln </a> </span> <?js } ?> <span class="flags flags-{item.flags}">{p.Stream.FLAG_NAME[item.flags]}</span>');
+		addStyle(extensionStyle);
 	});
 })();
