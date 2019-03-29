b2586d4931473e90a865b2c511518a5031644e36
holzmaster <holzmaster@outlook.com>
2019-03-30 00:59:57 +0100

    Make userscript work again

--- sf-status.user.js
+++ sf-status.user.js
@@ -5,7 +5,7 @@
 // @namespace	holzmaster
 // @include		http://pr0gramm.com*
 // @include		https://pr0gramm.com*
-// @version		1.1.6
+// @version		1.2.0
 // @updateURL	https://holzmaster.github.io/sf-status/sf-status.user.js
 // @downloadURL	https://holzmaster.github.io/sf-status/sf-status.user.js
 // @copyright	2015+, holzmaster
@@ -13,26 +13,33 @@
 // @grant		none
 // ==/UserScript==
 
-(function() {
-	"use strict";
-	var extensionStyle = ".flags { color: #FFF; padding: 0 0.5em 0.1em 0.45em; margin-left: 0.3em; vertical-align: baseline; text-transform: lowercase; text-align: center; text-shadow: 1px 1px 1px #7F7F7F; line-height: 1; font-weight: 700; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; border-radius: 0.25em; }\n";
-	extensionStyle += ".flags-1 { background-color: #5CB85C; }\n";
-	extensionStyle += ".flags-2 { background-color: #F0AD4E; }\n";
-	extensionStyle += ".flags-4 { background-color: #D9534F; }\n";
+(function () {
+	const s = document.createElement('script');
+	s.appendChild(document.createTextNode('(' + uneval(main) + ')();'));
+	(document.body || document.head || document.documentElement).appendChild(s);
 
-	function addStyle(css) {
-		var head = document.getElementsByTagName("head")[0];
-		if (!head) return;
-		var style = document.createElement("style");
-		style.type = "text/css";
-		style.innerHTML = css;
-		head.appendChild(style);
-	};
-	$(function() {
-		p.View.Stream.Item.prototype.template =
-		p.View.Stream.Item.prototype.template
-			.replace('melden</span>] <?js } ?>',
-					 'melden</span>] <?js } ?> <span class="flags flags-{item.flags}">{p.Stream.FLAG_NAME[item.flags]}</span>');
-		addStyle(extensionStyle);
-	});
+	function main() {
+		"use strict";
+		var extensionStyle = ".flags { color: #FFF; padding: 0 0.5em 0.1em 0.45em; margin-left: 0.3em; vertical-align: baseline; text-transform: lowercase; text-align: center; text-shadow: 1px 1px 1px #7F7F7F; line-height: 1; font-weight: 700; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; border-radius: 0.25em; }\n";
+		extensionStyle += ".flags-1 { background-color: #5CB85C; }\n";
+		extensionStyle += ".flags-2 { background-color: #F0AD4E; }\n";
+		extensionStyle += ".flags-4 { background-color: #D9534F; }\n";
+
+		function addStyle(css) {
+			var head = document.getElementsByTagName("head")[0];
+			if (!head) return;
+			var style = document.createElement("style");
+			style.type = "text/css";
+			style.innerHTML = css;
+			head.appendChild(style);
+		};
+		$(function () {
+			p.View.Stream.Item.prototype.template =
+				p.View.Stream.Item.prototype.template.replace(
+						'melden</span>] <?js } ?>',
+						'melden</span>] <?js } ?> <span class="flags flags-{item.flags}">{p.Stream.FLAG_NAME[item.flags]}</span>'
+				);
+			addStyle(extensionStyle);
+		});
+	}
 })();
