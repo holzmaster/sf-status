e8853413a553bf73808a1327ee92183f155910e7
holzmaster <holzmaster@outlook.com>
2020-05-04 12:04:03 +0200

    Modernize + NSFP style

--- sf-status.user.js
+++ sf-status.user.js
@@ -5,7 +5,7 @@
 // @namespace	holzmaster
 // @include		http://pr0gramm.com*
 // @include		https://pr0gramm.com*
-// @version		1.3.0
+// @version		1.4.0
 // @updateURL	https://holzmaster.github.io/sf-status/sf-status.user.js
 // @downloadURL	https://holzmaster.github.io/sf-status/sf-status.user.js
 // @copyright	2015+, holzmaster
@@ -14,30 +14,34 @@
 // ==/UserScript==
 
 (function () {
-	const s = document.createElement('script');
-	s.appendChild(document.createTextNode('(' + main.toString() + ')();'));
+	const s = document.createElement("script");
+	s.appendChild(document.createTextNode("(" + main.toString() + ")();"));
 	(document.body || document.head || document.documentElement).appendChild(s);
 
 	function main() {
 		"use strict";
-		var extensionStyle = ".flags { color: #FFF; padding: 0 0.5em 0.1em 0.45em; margin-left: 0.3em; vertical-align: baseline; text-transform: lowercase; text-align: center; text-shadow: 1px 1px 1px #7F7F7F; line-height: 1; font-weight: 700; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; border-radius: 0.25em; }\n";
-		extensionStyle += ".flags-1 { background-color: #5CB85C; }\n";
-		extensionStyle += ".flags-2 { background-color: #F0AD4E; }\n";
-		extensionStyle += ".flags-4 { background-color: #D9534F; }\n";
+		const extensionStyle = `
+		.flags { color: #fff; padding: 0 0.5em 0.1em 0.45em; margin-left: 0.3em; vertical-align: baseline; text-transform: lowercase; text-align: center; text-shadow: 1px 1px 1px #7F7F7F; line-height: 1; font-weight: 700; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; border-radius: 0.25em; }
+		.flags-1 { background-color: #5cb85c; }
+		.flags-2 { background-color: #f0ad4e; }
+		.flags-4 { background-color: #d9534f; }
+		.flags-8 { background-color: #004085; }
+		`;
 
 		function addStyle(css) {
-			var head = document.getElementsByTagName("head")[0];
+			const head = document.getElementsByTagName("head")[0];
 			if (!head) return;
-			var style = document.createElement("style");
+			const style = document.createElement("style");
 			style.type = "text/css";
 			style.innerHTML = css;
 			head.appendChild(style);
 		};
+
 		$(function () {
 			p.View.Stream.Item.prototype.template =
 				p.View.Stream.Item.prototype.template.replace(
-						'melden</span>] <?js } ?>',
-						'melden</span>] <?js } ?> <span class="flags flags-{item.flags}">{p.Stream.FLAG_NAME[item.flags]}</span>'
+						`melden</span>] <?js } ?>`,
+						`melden</span>] <?js } ?> <span class="flags flags-{item.flags}">{p.Stream.FLAG_NAME[item.flags]}</span>`
 				);
 			addStyle(extensionStyle);
 		});
